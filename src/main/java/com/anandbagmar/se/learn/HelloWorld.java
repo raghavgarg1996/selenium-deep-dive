518ee853f47d278cda933e0727954f9c9cad7d6e
Anand Bagmar <abagmar@gmail.com>
2020-09-06 12:49:02 +0530

    auto-indent code

--- src/main/java/com/anandbagmar/se/learn/HelloWorld.java
+++ src/main/java/com/anandbagmar/se/learn/HelloWorld.java
@@ -5,11 +5,11 @@ public class HelloWorld {
         System.out.println("HelloWorld instantiated");
     }
 
-    public String getDefaultMessage() {
-        return this.getClass().getSimpleName();
-    }
-
     public static void main(String[] args) {
         System.out.println(new HelloWorld().getDefaultMessage());
+    }
+
+    public String getDefaultMessage() {
+        return this.getClass().getSimpleName();
     }
 }

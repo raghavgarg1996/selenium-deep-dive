c9ff56e7d3438aad0741b9627cbc0747eaf9f56a
Shama Ugale <Shama.Ugale@gmail.com>
2022-07-27 01:17:03 +0530

    added full screenshot test

--- src/main/java/com/se4/module7/Screenshots.java
+++ src/main/java/com/se4/module7/Screenshots.java
@@ -2,19 +2,12 @@ package com.se4.module7;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class Screenshots {
 
@@ -22,24 +15,43 @@ public class Screenshots {
     final static String PROJECT_PATH = System.getProperty("user.dir");
 
     @BeforeTest
-    public void setup(){
+    public void setup() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
     }
 
 
     @Test
-    public void loginTest() throws IOException {
+    public void loginTest() throws Exception {
         driver.get("https://www.google.com");
 
         // take screenshot of an element
         WebElement logo = driver.findElement(By.xpath("//img[@alt='Google']"));
-        File file = logo.getScreenshotAs(OutputType.FILE);
-        File destFile = new File(PROJECT_PATH + "/target/screenshots/logo.png");
-        FileUtils.copyFile(file, destFile);
+        takeElementScreenshot(logo, PROJECT_PATH + "/target/screenshots/logo.png");
 
+        // take screenshot of the entire page
+        takeFullPageScreenshot(driver, PROJECT_PATH + "/target/screenshots/fullPage.png");
         driver.quit();
     }
 
+    public static void takeFullPageScreenshot(WebDriver webdriver, String fileWithPath) throws Exception {
+        //Convert web driver object to TakeScreenshot
+        TakesScreenshot scrShot = ((TakesScreenshot) webdriver);
+
+        //Call getScreenshotAs method to create image file
+        File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
+
+        //Move image file to new destination
+        File DestFile = new File(fileWithPath);
+
+        //Copy file at destination
+        FileUtils.copyFile(SrcFile, DestFile);
+    }
+
+    public static void takeElementScreenshot(WebElement element, String filePath) throws Exception {
+        File file = element.getScreenshotAs(OutputType.FILE);
+        File destFile = new File(filePath);
+        FileUtils.copyFile(file, destFile);
+    }
 
 }

ae27059dd5e82671b73ba0d8e89d3d0d20099c5a
Shama Ugale <Shama.Ugale@gmail.com>
2022-07-27 01:18:43 +0530

    added waits class

---
+++ src/main/java/com/se4/module11/Waits.java
@@ -0,0 +1,16 @@
+package com.se4.module11;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+
+public class Waits {
+    WebDriver driver;
+
+    @BeforeTest
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+}

a4847ab347afc885f4070e76868efa72f95c0d4b
Anand Bagmar <abagmar@gmail.com>
2022-07-27 12:53:32 +0530

    formatted remaining tests

--- src/main/java/com/se4/BreakingChanges.java
+++ src/main/java/com/se4/BreakingChanges.java
@@ -9,32 +9,31 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 import java.util.NoSuchElementException;
-import java.util.concurrent.TimeUnit;
 
 public class BreakingChanges {
     WebDriver driver;
 
     @BeforeMethod
-    public void setup(){
-        WebDriverManager.chromedriver().setup();
+    public void setup() {
+        WebDriverManager.chromedriver()
+                        .setup();
         driver = new ChromeDriver();
     }
 
     @Test
-    public void breakingChangesTest(){
+    public void breakingChangesTest() {
         // selenium 3 usage
         // uncomment to see the breaking change
 
-//        FluentWait waitSe3 = new FluentWait(driver)
-//                .pollingEvery(20, TimeUnit.MILLISECONDS)
-//                .withTimeout(20, TimeUnit.SECONDS)
-//                .ignoring(NoSuchElementException.class);
+        //        FluentWait waitSe3 = new FluentWait(driver)
+        //                .pollingEvery(20, TimeUnit.MILLISECONDS)
+        //                .withTimeout(20, TimeUnit.SECONDS)
+        //                .ignoring(NoSuchElementException.class);
 
-//selenium 4 usage
-        FluentWait waitSe4 = new FluentWait(driver)
-                .pollingEvery(Duration.ofMillis(500))
-                .ignoring(NoSuchElementException.class)
-                .withTimeout(Duration.ofSeconds(60));
+        //selenium 4 usage
+        FluentWait waitSe4 = new FluentWait(driver).pollingEvery(Duration.ofMillis(500))
+                                                   .ignoring(NoSuchElementException.class)
+                                                   .withTimeout(Duration.ofSeconds(60));
 
     }
 }
--- src/main/java/com/se4/Deprecated.java
+++ src/main/java/com/se4/Deprecated.java
@@ -1,78 +1,82 @@
 package com.se4;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.anandbagmar.driver.Driver;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.FluentWait;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.concurrent.TimeUnit;
 
 public class Deprecated {
     WebDriver driver;
 
     @BeforeMethod
-    public void setup(){
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+    public void setup() {
+        driver = new Driver().createChromeDriver();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
     }
 
     @Test
-    public void deprecatedMethods(){
+    public void deprecatedMethods() {
         driver.get("https://www.google.com");
 
         // findElement
-        WebElement serachTxtBox = driver.findElement(By.name("q"));
+        WebElement searchTextBox = driver.findElement(By.name("q"));
 
         //findElements
         List<WebElement> allLinks = driver.findElements(By.tagName("a"));
 
 
         // Selenium 3 usage
-        Actions act = new Actions(driver);
-        WebElement toDoList= driver.findElement(By.id("toDoListBtn"));
+        Actions se3Actions = new Actions(driver);
+        WebElement toDoList = driver.findElement(By.id("toDoListBtn"));
 
         // click method - to click on a webElement
-        act.moveToElement(toDoList).click();
+        se3Actions.moveToElement(toDoList)
+                  .click();
 
         // double click - double click on a webElement
-        act.moveToElement(toDoList).doubleClick();
+        se3Actions.moveToElement(toDoList)
+                  .doubleClick();
 
         // Context click - Right click on a webElement
-        act.moveToElement(toDoList).contextClick();
+        se3Actions.moveToElement(toDoList)
+                  .contextClick();
 
         //clickAndHold method - click and hold on a webElement without releasing
-        act.moveToElement(toDoList).clickAndHold();
+        se3Actions.moveToElement(toDoList)
+                  .clickAndHold();
 
         // release -release the hold on a webElement
-        act.moveToElement(toDoList).release();
+        se3Actions.moveToElement(toDoList)
+                  .release();
 
         // Selenium 4 usage
-        Actions act1 = new Actions(driver);
-        WebElement toDoList1= driver.findElement(By.id("toDoListBtn"));
+        Actions se4Actions = new Actions(driver);
+        WebElement toDoList1 = driver.findElement(By.id("toDoListBtn"));
 
         // click method - to click on a webElement
-        act.click(toDoList);
+        se4Actions.click(toDoList);
 
         //clickAndHold method - click and hold on a webElement without releasing
-        act.clickAndHold(toDoList);
+        se4Actions.clickAndHold(toDoList);
 
         // Context click - Right click on a webElement
-        act.contextClick(toDoList);
+        se4Actions.contextClick(toDoList);
 
         // double click - double click on a webElement
-        act.doubleClick(toDoList);
+        se4Actions.doubleClick(toDoList);
 
         // release -release the hold on a webElement
-        act.release(toDoList);
+        se4Actions.release(toDoList);
 
     }
 }
